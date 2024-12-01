a0c84a721d2e8f4018eb50d4010da9d412e33ada
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-12-01 07:13:51 +0000

    create simple form for login

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -2,6 +2,20 @@ export default function Login(){
     return(
         <div>
             <h1> Login here </h1>
+            <table>
+                <tr>
+                    <td> Username: </td>
+                    <td> <input type="text" name="username" /></td>
+                </tr>
+                <tr>
+                    <td> Password: </td>
+                    <td> <input type="password" name="pwd" /> </td>
+                </tr>
+                <tr>
+                    <td style={{color: 'red'}} colSpan={2}> {user.error} </td>
+                </tr>
+            </table>
+            <button> Login </button>
         </div>
     )
 }
